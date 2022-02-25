d3f02e328e2a2a1fdf8f6a3969de154f699424e8
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2022-02-25 14:26:42 +0500

    Initial Commit

---
+++ iOScalculator/iOScalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOScalculatorApp.swift
+//  iOScalculator
+//
+//  Created by GAMA  on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOScalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
